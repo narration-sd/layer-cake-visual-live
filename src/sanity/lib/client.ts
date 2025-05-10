86453b11b77764e09c00314b5fa9b9fc3df0c0a4
Narration SD <narration-sd@users.noreply.github.com>
2025-05-09 17:04:45 -0700

    And set the default perspective for website, tested to still alter correctly with Visual Editing, actually remote.

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,6 +6,7 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
+  perspective: 'published',
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
   // stega: { studioUrl: '/studio' },
   stega: { studioUrl: 'https://sa-gnu.netlify.app/studio' },
