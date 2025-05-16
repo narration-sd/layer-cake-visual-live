75068b57e8d05a7921a2172a5f587ef6efee70b0
Narration SD <narration-sd@users.noreply.github.com>
2025-05-15 17:18:47 -0700

    well, this error disappeared from ide, but it's back and breaking vercel build.

--- sanity.config.ts
+++ sanity.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
       resolve,
       previewUrl: {
         previewMode: {
+          // @ts-expect-error somehow this is in code but not in ts defines yet
           initial: previewUrl,
           enable: '/api/draft-mode/enable',
         },
