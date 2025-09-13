13466a01aae5e1248acaba0d4e447d567bdec508
Narration SD <narration-sd@users.noreply.github.com>
2025-09-12 17:34:47 -0700

    latest ncu -u, except holding back on taliwindcss leap. Made mods for next-sanity 11, and had to remove the --turbopack from the dev script to avoid a fatal turbopack error which has been multiply reported with no resolution on the turbopack site.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,5 @@
 import { draftMode } from "next/headers";
-import { VisualEditing } from "next-sanity";
+import { VisualEditing } from "next-sanity/visual-editing";
 import { DisableDraftMode } from "@/components/DisableDraftMode";
 import { Header } from "@/components/Header";
 import { SanityLive } from "@/sanity/lib/live";
